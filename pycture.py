c509c171d4b7b726c015ee3443e6c3a0dd93234d
Armin Abbasi <apmnh.armin@gmail.com>
2023-01-02 00:48:14 +0100

    Add feature

--- pycture.py
+++ pycture.py
@@ -3,33 +3,26 @@ from PIL import Image, ImageColor
 from pathlib import Path
 
 
-class Pycture():
-    def __init__(self, path: Path, tdir: Path = "resized") -> None:
-        self.tdir: Path = path.parent / tdir
-        if not tdir.exists():
-            tdir.mkdir()
+def resize(path: Path, size: Tuple) -> None:
+    img = Image.open(path)
+    width, height = size
 
-        self.img = Image.open(path)
-        self.path = path
-
-    def resize(self, size: Tuple) -> None:
-        width, height = size
-
-        self.img.resize((width, height)).save(self.tdir / path.name)
-
-    def crop(self, box: Tuple) -> None:
-        left, top, right, bottom = box
-
-        self.img.crop((left, top, right, bottom)).save(self.tdir / path.name)
+    img.resize((width, height)).save(tdir / path.name)
 
 
-path = Path(
+def crop(path: Path, box: Tuple) -> None:
+    img = Image.open(path)
+    left, top, right, bottom = box
+
+    img.crop((left, top, right, bottom)).save(tdir / path.name)
+
+
+parent_path = Path(
     "/home/armin/Repository/Businesses/Web Business/Clients/Silk Flower International/Desktop")
 
-# tdir = "resized"
+tdir: Path = parent_path / "resized"
+if not tdir.exists():
+    tdir.mkdir()
 
-# for images in path.glob("*.*"):
-#     Pycture(images, tdir).crop((50, 50, 50, 50))
-
-img = Image.open(next(path.glob("*.*")))
-print(img.size)
+for images in parent_path.glob("*.*"):
+    crop(images, (20, 20, 100, 100))
