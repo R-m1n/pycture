f8983bc27a695a53fbc3d9f6332aaa30eb0b11c4
Armin Abbasi <apmnh.armin@gmail.com>
2023-01-02 14:48:00 +0100

    Add feature

--- pycture.py
+++ pycture.py
@@ -4,69 +4,70 @@ from PIL import Image, ImageColor
 from pathlib import Path
 
 
-def resize(path: Path, size: Tuple) -> None:
-    img = Image.open(path)
+def resize(img: Image.Image, size: Tuple) -> Image.Image:
     width, height = size
 
-    img.resize((width, height)).save(tdir / path.name)
+    return img.resize((width, height))
 
 
-def crop(path: Path, box: Tuple) -> None:
-    img = Image.open(path)
+def crop(img: Image.Image, box: Tuple) -> Image.Image:
     left, top, right, bottom = box
 
-    img.crop((left, top, right, bottom)).save(tdir / path.name)
+    return img.crop((left, top, right, bottom))
 
 
-def overlay(path: Path, rgba: Tuple) -> None:
-    img = Image.open(path).convert("RGBA")
-    red, green, blue, alpha = rgba
+def overlay(img: Image.Image, rgba: Tuple) -> Image.Image:
+    overlay = Image.new("RGBA", img.size, rgba)
+
+    img.paste(overlay, mask=overlay)
+    return img
+
+
+def remove_bg(img: Image.Image) -> Image.Image:
+    img = img.convert("RGBA")
     width, height = img.size[0], img.size[1]
+    bg_color = img.getpixel((0, 0))
 
-    for x in range(1, width):
-        for y in range(1, height):
-            img.putpixel((x, y), (red, green, blue, alpha))
-
-    img.save(tdir / path.name)
-
-
-def remove_bg(path: Path) -> None:
-    img = Image.open(path).convert("RGBA")
-    width, height = img.size[0], img.size[1]
-
-    colors = dict()
-    for x in range(1, width):
-        for y in range(1, height):
-            if img.getpixel((x, y)) not in colors:
-                colors[img.getpixel((x, y))] = 0
-
-            else:
-                colors[img.getpixel((x, y))] += 1
-
-    bg_color = sorted(
-        colors.items(), key=lambda item: item[0], reverse=True)[0][0]
-
-    for x in range(1, width):
-        for y in range(1, height):
+    for x in range(width):
+        for y in range(height):
             if img.getpixel((x, y)) == bg_color:
                 img.putpixel((x, y), (0))
 
-    img.save(tdir / path.name.replace(path.suffix, ".png"))
+    return img
+
+
+def watermark(img: Image.Image, logo: Image.Image, logo_box: Tuple) -> Image.Image:
+    width, height = img.size[0], img.size[1]
+
+    logo = remove_bg(resize(logo, logo_box))
+    logo_width, logo_height = logo.size[0], logo.size[1]
+    paste_position = (width - logo_width, height - logo_height)
+
+    img.paste(logo, paste_position, logo)
+    return img
 
 
 if __name__ == "__main__":
     path = Path(
-        "/home/armin/Repository/Businesses/Web Business/Clients/Silk Flower International/Desktop/photo_2022-11-27_11-32-46.jpg")
+        "/home/armin/Repository/Businesses/Web Business/Clients/Silk Flower International/Desktop")
+
+    logo = Path(
+        "/home/armin/Repository/Businesses/Web Business/Clients/Silk Flower International/Desktop/photo_2022-11-27_11-33-36.jpg")
 
     tdir = Path("resized")
 
-    tdir = path.parent / tdir
+    tdir = path / tdir
     if not tdir.exists():
         tdir.mkdir()
 
+    images = []
     if path.is_dir():
-        for images in path.glob("*.*"):
-            remove_bg(images)
+        for image in path.glob("*.*"):
+            images.append(image)
 
     else:
-        remove_bg(path)
+        images.append(path)
+
+    for image in images:
+        watermark(Image.open(image), Image.open(logo),
+                  (300, 300)).save(tdir / image.name)
